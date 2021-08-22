e9823db8b4987872da3feb1ccf007ebfc75b5a86
Volkov Nikolai <volkov@Volkov.local>
2021-08-22 18:30:25 +0300

    Docker compose project

--- Docker/my_flask_app/app.py
+++ Docker/my_flask_app/app.py
@@ -2,18 +2,19 @@
 # -*- coding: utf-8 -*-
 from flask import Flask, render_template, request
 import requests
-import json
 import os
+import psycopg2
+import dotenv
 
 app = Flask(__name__)
 
 @app.route('/', methods = ["GET", "POST"])
 def index():
-    name = os.environ['USER']
+    name = os.environ["USER"]
     if request.method == "POST":
         city = request.form["city"]
         country = request.form["country"]
-        api_key = os.environ['API_KEY']
+        api_key = os.environ["API_KEY"]
         
         weather_url = requests.get(f"http://api.openweathermap.org/data/2.5/weather?appid={api_key}&q={city},{country}&units=metric")
         weather_data = weather_url.json()
@@ -21,8 +22,41 @@ def index():
         temp = round(weather_data["main"]["temp"])
         humidity = weather_data["main"]["humidity"]
         wind_speed = weather_data["wind"]["speed"]
+        write_db(country, city, temp)
         return render_template("result.html", temp = temp , humidity = humidity, wind_speed = wind_speed, city = city)
     return render_template("index.html", name = name)
+
+con = psycopg2.connect(
+    host = "postgres",
+    database = "postgres",
+    user = "postgres",
+    password = "password",
+    port = 5432
+    )
+
+@app.before_first_request
+def create_table():
+
+    cur = con.cursor()
+
+    sql ='''CREATE TABLE IF NOT EXISTS Weather(
+            Id SERIAL PRIMARY KEY,
+            COUNTRY VARCHAR(40),
+            CITY VARCHAR(40),
+            TEMP INT)'''
+
+    cur.execute(sql)
+    con.commit()
+    cur.close()
+
+def write_db(country, city, temp):
+
+    cur = con.cursor()
+    
+    data = "INSERT INTO weather VALUES (DEFAULT, '%s','%s','%s');" % (country, city, temp)
+    cur.execute(data)
+    con.commit()
+    cur.close()
 
 
 if __name__ == '__main__':
