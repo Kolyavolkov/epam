0c8fb2a8e87e8d07164b0e16f1b256a9316c635e
Volkov Nikolai <volkov@Volkov.local>
2021-08-27 23:25:34 +0300

    flake8

--- Docker/my_flask_app/app.py
+++ Docker/my_flask_app/app.py
@@ -15,8 +15,8 @@ def index():
         city = request.form["city"]
         country = request.form["country"]
         api_key = os.environ["API_KEY"]
-        url = (f"http://api.openweathermap.org/data/2.5/"
-                "weather?appid={api_key}&q={city},{country}&units=metric")
+        url = ("http://api.openweathermap.org/data/2.5/"
+            f"weather?appid={api_key}&q={city},{country}&units=metric")
         weather_url = requests.get(url)
         weather_data = weather_url.json()
 
@@ -24,8 +24,8 @@ def index():
         humidity = weather_data["main"]["humidity"]
         wind_speed = weather_data["wind"]["speed"]
         write_db(country, city, temp)
-        return render_template("result.html", temp=temp ,humidity=humidity,
-                                wind_speed=wind_speed, city=city)
+        return render_template("result.html", temp=temp, humidity=humidity,
+            wind_speed=wind_speed, city=city)
     return render_template("index.html", name=name)
 
 
@@ -43,7 +43,7 @@ def create_table():
 
     cur = con.cursor()
 
-    sql ='''CREATE TABLE IF NOT EXISTS Weather(
+    sql = '''CREATE TABLE IF NOT EXISTS Weather(
             Id SERIAL PRIMARY KEY,
             COUNTRY VARCHAR(40),
             CITY VARCHAR(40),
@@ -57,8 +57,9 @@ def create_table():
 def write_db(country, city, temp):
 
     cur = con.cursor()
-    
-    data = "INSERT INTO weather VALUES (DEFAULT, '%s','%s','%s');" % (country, city, temp)
+
+    data = "INSERT INTO weather VALUES(DEFAULT, '%s','%s','%s');" %(
+        country, city, temp)
     cur.execute(data)
     con.commit()
     cur.close()
