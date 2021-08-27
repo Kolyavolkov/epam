a1bef79a250781973a82eb0be4027c22e62db65d
Volkov Nikolai <volkov@Volkov.local>
2021-08-27 23:16:05 +0300

    flake8

--- Docker/my_flask_app/app.py
+++ Docker/my_flask_app/app.py
@@ -4,26 +4,28 @@ import os
 from flask import Flask, render_template, request
 import requests
 import psycopg2
-import dotenv
 
 app = Flask(__name__)
 
-@app.route('/', methods = ["GET", "POST"])
+
+@app.route('/', methods=["GET", "POST"])
 def index():
     name = os.environ["USER"]
     if request.method == "POST":
         city = request.form["city"]
         country = request.form["country"]
         api_key = os.environ["API_KEY"]
-
-        weather_url = requests.get(f"http://api.openweathermap.org/data/2.5/weather?appid={api_key}&q={city},{country}&units=metric")
+        url = (f"http://api.openweathermap.org/data/2.5/"
+                "weather?appid={api_key}&q={city},{country}&units=metric")
+        weather_url = requests.get(url)
         weather_data = weather_url.json()
 
         temp = round(weather_data["main"]["temp"])
         humidity = weather_data["main"]["humidity"]
         wind_speed = weather_data["wind"]["speed"]
         write_db(country, city, temp)
-        return render_template("result.html", temp = temp , humidity = humidity, wind_speed = wind_speed, city = city)
+        return render_template("result.html", temp=temp ,humidity=humidity,
+                                wind_speed=wind_speed, city=city)
     return render_template("index.html", name=name)
 
 
@@ -39,7 +41,7 @@ con = psycopg2.connect(
 @app.before_first_request
 def create_table():
 
-    cur=con.cursor()
+    cur = con.cursor()
 
     sql ='''CREATE TABLE IF NOT EXISTS Weather(
             Id SERIAL PRIMARY KEY,
