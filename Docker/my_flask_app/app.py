6434411dbb5b54c6c3a9c4882feb4ad874cae768
Volkov Nikolai <volkov@Volkov.local>
2021-08-28 12:36:10 +0300

    added pre-commit check

--- Docker/my_flask_app/app.py
+++ Docker/my_flask_app/app.py
@@ -8,15 +8,17 @@ import psycopg2
 app = Flask(__name__)
 
 
-@app.route('/', methods=["GET", "POST"])
+@app.route("/", methods=["GET", "POST"])
 def index():
     name = os.environ["USER"]
     if request.method == "POST":
         city = request.form["city"]
         country = request.form["country"]
         api_key = os.environ["API_KEY"]
-        url = ("http://api.openweathermap.org/data/2.5/"
-               f"weather?appid={api_key}&q={city},{country}&units=metric")
+        url = (
+            "http://api.openweathermap.org/data/2.5/"
+            f"weather?appid={api_key}&q={city},{country}&units=metric"
+        )
         weather_url = requests.get(url)
         weather_data = weather_url.json()
 
@@ -24,8 +26,13 @@ def index():
         humidity = weather_data["main"]["humidity"]
         wind_speed = weather_data["wind"]["speed"]
         write_db(country, city, temp)
-        return render_template("result.html", temp=temp, humidity=humidity,
-                               wind_speed=wind_speed, city=city)
+        return render_template(
+            "result.html",
+            temp=temp,
+            humidity=humidity,
+            wind_speed=wind_speed,
+            city=city,
+        )
     return render_template("index.html", name=name)
 
 
@@ -34,8 +41,8 @@ con = psycopg2.connect(
     database=os.environ["POSTGRES_USER"],
     user=os.environ["POSTGRES_USER"],
     password=os.environ["POSTGRES_PASSWORD"],
-    port=5432
-    )
+    port=5432,
+)
 
 
 @app.before_first_request
@@ -43,11 +50,11 @@ def create_table():
 
     cur = con.cursor()
 
-    sql = '''CREATE TABLE IF NOT EXISTS Weather(
+    sql = """CREATE TABLE IF NOT EXISTS Weather(
             Id SERIAL PRIMARY KEY,
             COUNTRY VARCHAR(40),
             CITY VARCHAR(40),
-            TEMP INT)'''
+            TEMP INT)"""
 
     cur.execute(sql)
     con.commit()
@@ -59,11 +66,14 @@ def write_db(country, city, temp):
     cur = con.cursor()
 
     data = "INSERT INTO weather VALUES(DEFAULT, '%s','%s','%s');" % (
-        country, city, temp)
+        country,
+        city,
+        temp,
+    )
     cur.execute(data)
     con.commit()
     cur.close()
 
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
